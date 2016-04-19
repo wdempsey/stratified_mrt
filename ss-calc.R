1dd7d5c03ad8ab65c1942825c30d897871dc19ea
wdempsey <dempsey.walter@gmail.com>
2016-04-19 15:50:23 -0400

    Updated to do par for ss-calc

--- ss-calc.R
+++ ss-calc.R
@@ -2,15 +2,15 @@ source("./functions.R"); source("./setup.R")
 
 num.iters = 2000
 x = 1
-sim.output1 = foreach(i=1:num.iters, .combine = "cbind") %do% rho.function(x,N, pi, tau, P.0, P.treat, T, window.length, min.p, max.p)
+sim.output1 = foreach(i=1:num.iters, .combine = "cbind") %dopar% rho.function(x,N, pi, tau, P.0, P.treat, T, windoparw.length, min.p, max.p)
 x = 2
-sim.output2 = foreach(i=1:num.iters, .combine = "cbind") %do% rho.function(x,N, pi, tau, P.0, P.treat, T, window.length, min.p, max.p)
+sim.output2 = foreach(i=1:num.iters, .combine = "cbind") %dopar% rho.function(x,N, pi, tau, P.0, P.treat, T, windoparw.length, min.p, max.p)
 
-mean.rho.t1 = foreach(i=1:720, .combine = "c") %do% mean(sim.output1[i,sim.output1[i,]!=0])
-mean.rho.t2 = foreach(i=1:720, .combine = "c") %do% mean(sim.output2[i,sim.output2[i,]!=0])
+mean.rho.t1 = foreach(i=1:720, .combine = "c") %dopar% mean(sim.output1[i,sim.output1[i,]!=0])
+mean.rho.t2 = foreach(i=1:720, .combine = "c") %dopar% mean(sim.output2[i,sim.output2[i,]!=0])
 
-invvar.rho.t1 = foreach(i=1:720, .combine = "c") %do% mean(1/(sim.output1[i,sim.output1[i,]!=0]*(1-sim.output1[i,sim.output1[i,]!=0])))
-invvar.rho.t2 = foreach(i=1:720, .combine = "c") %do% mean(1/(sim.output2[i,sim.output2[i,]!=0]*(1-sim.output2[i,sim.output2[i,]!=0])))
+invvar.rho.t1 = foreach(i=1:720, .combine = "c") %dopar% mean(1/(sim.output1[i,sim.output1[i,]!=0]*(1-sim.output1[i,sim.output1[i,]!=0])))
+invvar.rho.t2 = foreach(i=1:720, .combine = "c") %dopar% mean(1/(sim.output2[i,sim.output2[i,]!=0]*(1-sim.output2[i,sim.output2[i,]!=0])))
 
 par(mfrow = c(2,1), mar = c(2,4,1,1)+0.1)
 plot(1:720/60,mean.rho.t1, xlab = "", ylab = "Rho | S")
@@ -24,16 +24,16 @@ plot(1:720/60,invvar.rho.t2, xlab = "Time (in hours)", ylab = "f | NS")
 invvar.rho.t1[is.nan(invvar.rho.t1)] = mean(invvar.rho.t1,na.rm=TRUE)
 invvar.rho.t2[is.nan(invvar.rho.t2)] = mean(invvar.rho.t2,na.rm=TRUE)
 
-Z.t = Vectorize(cov.gen)(1:(days*T))
+Z.t = Vectorize(cov.gen)(1:(num.days*T))
 d = find.d(bar.d, init.d, max.d, Z.t, num.days)
 
-term1 = (rep(invvar.rho.t1,days))#*tau[1]*pi[1])
-middle.term1 = foreach(i=1:ncol(Z.t), .combine = "+") %do% (term1[i]*outer(Z.t[,i],Z.t[,i]))
+term1 = (rep(invvar.rho.t1,num.days))#*tau[1]*pi[1])
+middle.term1 = foreach(i=1:ncol(Z.t), .combine = "+") %dopar% (term1[i]*outer(Z.t[,i],Z.t[,i]))
 
-term2 = (rep(invvar.rho.t2,days))#*tau[2]*pi[2])
-middle.term2 = foreach(i=1:ncol(Z.t), .combine = "+") %do% (term2[i]*outer(Z.t[,i],Z.t[,i]))
+term2 = (rep(invvar.rho.t2,num.days))#*tau[2]*pi[2])
+middle.term2 = foreach(i=1:ncol(Z.t), .combine = "+") %dopar% (term2[i]*outer(Z.t[,i],Z.t[,i]))
 
-outer.term = foreach(j=1:ncol(Z.t), .combine = "+") %do% outer(Z.t[,j],Z.t[,j])
+outer.term = foreach(j=1:ncol(Z.t), .combine = "+") %dopar% outer(Z.t[,j],Z.t[,j])
 
 Sigma.1 = solve(outer.term,middle.term1)%*%solve(outer.term)/(sum(pi[1]*tau[1]))
 Sigma.2 = solve(outer.term,middle.term2)%*%solve(outer.term)/(sum(pi[2]*tau[2]))
@@ -42,9 +42,9 @@ Sigma.2 = solve(outer.term,middle.term2)%*%solve(outer.term)/(sum(pi[2]*tau[2]))
 
 # num.iters = 500
 # x = 1
-# sigma.output1 = foreach(i=1:num.iters, .combine = "cbind") %do% var.function(x,N, pi, tau, P.0, P.treat, T, window.length, min.p, max.p)
+# sigma.output1 = foreach(i=1:num.iters, .combine = "cbind") %dopar% var.function(x,N, pi, tau, P.0, P.treat, T, windoparw.length, min.p, max.p)
 # x = 2
-# sigma.output2 = foreach(i=1:num.iters, .combine = "cbind") %do% var.function(x,N, pi, tau, P.0, P.treat, T, window.length, min.p, max.p)
+# sigma.output2 = foreach(i=1:num.iters, .combine = "cbind") %dopar% var.function(x,N, pi, tau, P.0, P.treat, T, windoparw.length, min.p, max.p)
 
 sigmasq.1 = 0.004321966 #mean(sigma.output1,na.rm = TRUE)
 sigmasq.2 = 0.003752347 #mean(sigma.output2,na.rm = TRUE)
