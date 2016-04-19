c1c430b10edb542cec1d8c5b25a9c858a686acfd
Walter Dempsey <dempsey.walter@gmail.com>
2016-04-19 11:26:38 -0400

    Updated files

--- estimation.R
+++ estimation.R
@@ -5,9 +5,7 @@ Z.t = Vectorize(cov.gen)((1:num.days) * T)
 d = find.d(bar.d,init.d,max.d, Z.t,num.days)
 daily.treat = t(Z.t)%*%d
 
-num.iters = 10
+num.iters = 200
 
-foreach(i=1:num.iters, .combine = c) %do% estimation.simulation(num.persons, N, pi, tau, P.0, daily.treat, T, window.length, min.p, max.p)
+small.study = foreach(i=1:num.iters, .combine = c) %do% estimation.simulation(num.persons, N, pi, tau, P.0, daily.treat, T, window.length, min.p, max.p)
 
-
-system.time(estimation.simulation(num.persons, N, pi, tau, P.0, daily.treat, T, window.length, min.p, max.p))
