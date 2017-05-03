76fe089bf76e32ef82e22850b92507486f7afd12
wdempsey <dempsey.walter@gmail.com>
2017-05-03 14:59:07 -0400

    Updated the P distribution

--- MD2K/Conditional Case/setup.R
+++ MD2K/Conditional Case/setup.R
@@ -8,14 +8,24 @@ num.days = 10 # Number of days the study will be conducted
 
 window.length = 60 # Window length for the calculating proximal outcome
 
-P = matrix(c(0.667,0.053,0.289,0.325), nrow = 2,byrow = TRUE)
-#P = matrix(c(0.85,0.15,0.47,0.53), nrow = 2,byrow = TRUE)
+# Construct Transition Matrix given Inputs 1 and 2
+bar.W = c(0.074, 0.53)
+bar.Z = c(10.3,11.8)
 
-P[1,] = P[1,]/sum(P[1,]); P[2,] = P[2,]/sum(P[2,])
+tilde.Z = c((bar.Z[1]-3)/2, 0,(bar.Z[1]-3)/2,
+(bar.Z[2]-3)/2, 0,(bar.Z[2]-3)/2)
+
+P  = matrix(0, nrow = 6, ncol = 6)
+
+diag(P) = tilde.Z/(1+tilde.Z)
+P[2,3] = P[5,6] = 1.0
+P[1,2] = 1-P[1,1]; P[4,5] = 1-P[4,4]
+P[3,1] = (1-bar.W[1]) * (1-P[3,3]); P[3,4] = bar.W[1] * (1-P[3,3])
+P[6,1] = (1-bar.W[2]) * (1-P[6,6]); P[6,4] = bar.W[2] * (1-P[6,6])
 
 eig.P = eigen(P)
 
-pi = (eig.P$vectors%*%diag(c(1,0))%*%solve(eig.P$vectors))[1,]  # Stationary distribution for P
+pi = (eig.P$vectors%*%diag(c(1,rep(0,5)))%*%solve(eig.P$vectors))[1,]  # Stationary distribution for P
 
 # tau = rep(0.1,2) # Expected availability in each group ("Stressed", "Not Stressed")
 
@@ -31,11 +41,4 @@ max.p = 0.999 # Maximum randomization probability at each time point
 init.d = 0 # Initial treatment effect
 max.d = num.days/2 # Day of maximum treatment effect
 bar.d = 0.01 # Avg treatment effect
-
-#### Construct daily treatment vector
-# source("./functions.R")
-# Z.t = Vectorize(cov.gen)((1:num.days) * T)
-# d = find.d(bar.d,init.d,max.d, Z.t,num.days)
-# daily.treat = -t(Z.t)%*%d
-
 
