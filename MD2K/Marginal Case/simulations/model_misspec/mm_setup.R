027b455f2a4c5e8a27ee58aa7e2e44fb9919efb5
wdempsey <dempsey.walter@gmail.com>
2017-05-24 15:55:55 -0400

    Updated mm_setup.R for Marg Case

--- MD2K/Marginal Case/simulations/model_misspec/mm_setup.R
+++ MD2K/Marginal Case/simulations/model_misspec/mm_setup.R
@@ -8,25 +8,41 @@ num.days = 10 # Number of days the study will be conducted
 
 window.length = 60 # Window length for the calculating proximal outcome
 
-P = matrix(c(0.667,0.053,0.289,0.325), nrow = 2,byrow = TRUE)
-#P = matrix(c(0.85,0.15,0.47,0.53), nrow = 2,byrow = TRUE)
+# Construct Transition Matrix given Inputs 1 and 2
+bar.W = c(0.074, 0.53)
+bar.Z = c(10.3,11.8)
 
-P[1,] = P[1,]/sum(P[1,]); P[2,] = P[2,]/sum(P[2,])
+temp = c(1,-1)
 
-eig.P = eigen(P)
+epsilons = c(0.01,2)
 
-pi = (eig.P$vectors%*%diag(c(1,0))%*%solve(eig.P$vectors))[1,]  # Stationary distribution for P
+potential.errors = expand.grid(temp,temp,temp,temp)
 
-P.weekend = P; P.weekend[2,] = c(0.6,0.4)
+P.list = list(); pi.list = list()
+temp.barW = bar.W
+temp.barZ = bar.Z
 
-eig.Pwkend = eigen(P.weekend)
+source("./mm_functions.R")
 
-pi.wkend = (eig.Pwkend$vectors%*%diag(c(1,0))%*%solve(eig.Pwkend$vectors))[1,]  # Stationary distribution for P
+for (i in 1:16) {
+    temp.barW[1] = bar.W[1] + epsilons[1]*potential.errors[i,1]
+    temp.barW[2] = bar.W[2] + epsilons[1]*potential.errors[i,2]
 
-##tau = rep(0.1,2) # Expected availability in each group ("Stressed", "Not Stressed")
+    temp.barZ[1] = bar.Z[1] + epsilons[2]*potential.errors[i,3]
+    temp.barZ[2] = bar.Z[2] + epsilons[2]*potential.errors[i,4]
+
+    P = alt.calculateP(temp.barW,temp.barZ)
+
+    P.list[[i]] = P
+
+    approx.pi = (P%^%1000)[1,]
+
+    pi.list[[i]] = approx.pi
+
+}
 
 # Randomization probability inputs
-N =  c(1.5,1.5) # Avg. number of actions per day in each group ("Stressed", "Not Stressed")
+N =  c(0,1.61,0,0,2.05,0) # Avg. number of actions per day in each group ("Stressed", "Not Stressed")
 
 lambda = 0.3 # Smoothing parameter in the randomization formula
 
@@ -38,10 +54,7 @@ init.d = 0 # Initial treatment effect
 max.d = num.days/2 # Day of maximum treatment effect
 bar.d = 0.01 # Avg treatment effect
 
-#### Construct daily treatment vector
-##source("./functions.R")
-##Z.t = Vectorize(cov.gen)((1:num.days) * T)
-##d = find.d(bar.d,init.d,max.d, Z.t,num.days)
-##daily.treat = -t(Z.t)%*%d
-
+## Initial inputs for finding the closest P.treat to give you the
+## correct treatment effect.
+init.inputs = c(P[1,1],P[3,3],P[3,4]/(1-P[3,3]), P[4,4], P[6,6], P[6,4]/(1-P[6,6]))
 
