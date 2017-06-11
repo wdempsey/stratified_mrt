34653180ac7008254ba8eab6cb238904fdda12eb
wdempsey <dempsey.walter@gmail.com>
2017-06-11 13:41:41 -0400

    Prior to submission versions

--- MD2K/Conditional Case/std_effects.R
+++ MD2K/Conditional Case/std_effects.R
@@ -7,7 +7,7 @@ ss = power = vector(length = length(bar.beta.set))
 
 for(i in 1:length(bar.beta.set)) {
 
-    ### Treatment vector
+    ## Treatment vector
     Z.t = Vectorize(cov.gen)((1:num.days) * T)
     d = find.d(bar.beta.set[i],init.d,max.d,Z.t,num.days)
     daily.treat = -t(Z.t)%*%d
@@ -21,11 +21,9 @@ for(i in 1:length(bar.beta.set)) {
     }
 
     ## Calculate Sample Size
-    num.iters = 100
+    num.iters = 250
+    set.seed(1401409)
     test.sigma = barsigma.estimation(num.iters, N, pi, P, P.treat.list, T,
                                 window.length, min.p, max.p)
-    ## test.tildepr = tildepr.estimation(num.iters, N, pi, tau, P, daily.treat, T, window.length, min.p, max.p)
     print(c(bar.beta.set[i], round(bar.beta.set[i]/sqrt(test.sigma[,1]/test.sigma[,2]),4)))
-    ## print(c(bar.beta.set[i], tau.set[j], test.tildepr[,1]/test.tildepr[,2]))
-  }
 }
